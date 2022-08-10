a28cc98da219cef6ed62f01c5c8bc00b8909bad3
jayee28 <jayeebarman2@gmail.com>
2022-08-10 16:18:52 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
 #Declaring variables for setup functions
@@ -6,7 +6,7 @@ PROJECT_NAME="housing-predictor"
 VERSION="0.0.3"
 AUTHOR="Jayeeta Barman"
 DESRCIPTION="This is a Machine Learning Project"
-PACKAGES=["housing"]
+
 REQUIREMENT_FILE_NAME="requirements.txt"
 
 
@@ -20,7 +20,7 @@ def get_requirements_list() -> List[str]:
     of libraries mentioned in requirements.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 
 
@@ -29,6 +29,6 @@ name=PROJECT_NAME,
 version=VERSION,
 author=AUTHOR,
 description=DESRCIPTION,
-packages=PACKAGES,
+packages=find_packages(),
 install_requires=get_requirements_list()
 )
