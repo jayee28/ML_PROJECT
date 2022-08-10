9daa781628eb0cfdae630fbac2e521998250013a
jayee28 <jayeebarman2@gmail.com>
2022-08-10 08:54:50 +0530

    updated

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods = ["GET","POST"])
 def index():
-    return "Starting ML Project"
+    return "CI CD Pipeline has been established"
 
 if __name__=="__main__":
     app.run(debug=True)
